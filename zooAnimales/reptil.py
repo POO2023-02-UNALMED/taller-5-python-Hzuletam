36a70a84f976977deeff8e59b9e702a84fe2ec8a
Hzuletam <142773639+Hzuletam@users.noreply.github.com>
2023-09-19 03:01:05 -0500

    Update reptil.py

--- zooAnimales/reptil.py
+++ zooAnimales/reptil.py
@@ -5,7 +5,7 @@ class Reptil(Animal):
     iguanas = 0
     serpientes = 0
 
-    def __init__(self, nombre, edad, habitat, genero, colorEscamas, largoCola):
+    def __init__(self, nombre, edad, habitat, genero, colorEscamas, largoCola)->None:
         super().__init__(nombre, edad, habitat, genero)
         self._colorEscamas = colorEscamas
         self._largoCola = colorEscamas
